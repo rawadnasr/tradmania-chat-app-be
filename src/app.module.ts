e8807cec473f159c646f37ed8f26221d77d9396f
rawad <rnasr@addbloom.com>
2024-01-21 14:24:05 +0200

    added mysql database and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { SocketGateway } from './socket/socket.gateway';
-import { MessagesGateway } from './gateways/messages/messages.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from 'db/data-source';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
   controllers: [AppController],
-  // providers: [AppService, SocketGateway],
-  providers: [AppService, MessagesGateway],
+  providers: [AppService],
 })
 export class AppModule {}
