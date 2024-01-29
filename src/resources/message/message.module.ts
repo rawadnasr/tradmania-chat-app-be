dc2df56992245d6b6a90b449d5318a20e3fb723c
rawad <rnasr@addbloom.com>
2024-01-29 22:03:49 +0200

    added create message service

--- src/resources/message/message.module.ts
+++ src/resources/message/message.module.ts
@@ -3,11 +3,19 @@ import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/message.entity';
+import { ConversationModule } from '../conversation/conversation.module';
+import { ConversationService } from '../conversation/conversation.service';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
+  imports: [
+    TypeOrmModule.forFeature([Message]),
+    ConversationModule,
+    UsersModule,
+  ],
   controllers: [MessageController],
-  providers: [MessageService],
+  providers: [MessageService, ConversationService, UsersService],
   exports: [TypeOrmModule],
 })
 export class MessageModule {}
